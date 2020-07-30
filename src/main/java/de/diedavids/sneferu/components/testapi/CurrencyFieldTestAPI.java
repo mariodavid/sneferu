b2e69387d8a04f29dd6859aca2aa2c55e56a4fc3
Mario David <mario@die-davids.de>
2020-07-30 15:49:03 +0200

    added currency field (#2)

---
+++ src/main/java/de/diedavids/sneferu/components/testapi/CurrencyFieldTestAPI.java
@@ -0,0 +1,12 @@
+package de.diedavids.sneferu.components.testapi;
+
+import com.haulmont.cuba.gui.components.CurrencyField;
+import de.diedavids.sneferu.components.testapi.GenericComponentTestAPI;
+
+public class CurrencyFieldTestAPI extends GenericComponentTestAPI<CurrencyField> {
+
+    public CurrencyFieldTestAPI(CurrencyField component) {
+        super(component);
+    }
+
+}
